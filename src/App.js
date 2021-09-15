ea2097fe386a932355870a7279cc50ccd51a0544
Asther Louie Cabardo <67548363+ashcbrd@users.noreply.github.com>
2021-09-15 11:54:42 +0800

    delpoy

--- src/App.js
+++ src/App.js
@@ -80,7 +80,7 @@ class App extends Component {
 
   onSubmit = () => {
     this.setState({imageUrl: this.state.input})
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://agile-forest-39359.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -90,7 +90,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://agile-forest-39359.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
